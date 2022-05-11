b487c7f2fa52c49fc6be16adb4400bad12a355ae
RudrasreeLD <90783697+RudrasreeLD@users.noreply.github.com>
2022-05-11 21:18:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(635, 475);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("Rudru.png");
+}
